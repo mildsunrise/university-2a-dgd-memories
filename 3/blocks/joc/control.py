8d21898a43ca1d9f05ea7ba01cdbd3a9fec8d9ed
Xavier Mendez <me@jmendeth.com>
2016-12-18 16:38:06 +0100

    3: "control" block implementation

--- 3/blocks/joc/control.py
+++ 3/blocks/joc/control.py
@@ -37,7 +37,37 @@ El comportament del bloc no està definit si cap de les entrades $ngtx, neqx, nl
 '''
 
 implementation = ur'''
-% TODO
+Hem pres una forma poc usual de dissenyar aquest bloc: en comptes d'assignar
+$comp$ de forma combinacional fora del \mintinline{vhdl}|process|, es fa dins
+i només en certs casos (\emph{latch}). Hem vist que això simplifica molt el
+codi i ens ha permés fer el disseny en només \textbf{2 estats} (repós, jugant)
+en comptes de 5.
+
+El diagrama de transició d'estats (només depen de les entrades $coi$, $neqx$; sortides $eshft$, $ecnt$) és el següent. Es tracta
+d'una màquina de Mealy, però es representa la sortida dins de l'estat per simplicitat:
+
+\begin{center} \begin{tikzpicture}[shorten >=1pt, node distance=2cm, >=stealth', auto]
+  \node[state with output, initial above, initial text=reset] (st_idle) at (0,0) {idle \nodepart{lower} $0,1$};
+  \node[state with output]          (st_playing) at (4,0) {playing \nodepart{lower} $bcd,0$};
+
+  \path[->]
+    (st_idle) edge [loop left, align=center] node {$0,-$} (st_idle)
+    (st_idle) edge [bend left, align=center] node {$1,-$} (st_playing)
+    
+    (st_playing) edge [loop right, align=center] node {$0,0$} (st_playing)
+    (st_playing) edge [bend left, align=center] node {$1,-$ \\ $-,1$} (st_idle)
+  ;
+\end{tikzpicture} \end{center}
+
+Respecte a $comp$, se li assigna un valor només en els següents casos:
+
+\begin{itemize}
+\item En reset: \mintinline{vhdl}|"111"|
+\item En estat \emph{idle}, $coi$ s'activa: \mintinline{vhdl}|"000"|
+\item En estat \emph{playing}, $bcd$ s'activa: \mintinline{vhdl}|"000"|
+\item En estat \emph{playing}, $ast$ s'activa: \mintinline{vhdl}|ngtx & neqx & nltx|
+\item En estat \emph{playing}, $coi$ s'activa: \mintinline{vhdl}|"111"|
+\end{itemize}
 '''
 
 simulation = ur'''
